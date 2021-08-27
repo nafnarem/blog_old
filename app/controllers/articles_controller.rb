395af808f8b1ab1ee06aeedef41e71095e8e93f3
nafnarem <johnparreno@gmail.com>
2021-08-27 19:46:51 +0800

    Blog_test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,26 @@
 class ArticlesController < ApplicationController
 
-  def index
+  def index 
     @articles = Article.all
   end
 
   def show
-  
+    @article = Article.find(params[:id])
   end
-  
+
+  def find_name
+    @article = Article.find_by(name: params[:name])
+  end
+
   def new
     @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
-    # @article.name = params[:name]
-    # @article.body = params[:body]
-    if @article.save 
+    # @article.name = params[:name] 
+    # @article.body = params[:body] 
+    if @article.save
       redirect_to articles_path
     else
       render :new
@@ -24,21 +28,30 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-  
+    @article = Article.find(params[:id])
   end
 
   def update
-  
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to articles_path
+    else
+      render :edit
+    end
   end
 
   def delete
-  
+    @article = Article.find(params[:id])
+    if @article.destroy
+      redirect_to articles_path
+     else
+      render :index
+     end
   end
 
   private
 
-  def article_params
-    params.require(:article).permit(:name, :body)
-  end
-
+    def article_params
+      params.require(:article).permit(:name, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
   get '/articles' => 'articles#index'
   get '/articles/new' => 'articles#new'
+  get '/articles/name/:name' => 'articles#find_name'
+  get '/articles/:id' => 'articles#show', as: 'article_show'
+  delete '/articles/:id' => 'articles#delete', as: 'article_delete'
+  get '/articles/:id/edit' => 'articles#edit', as: 'article_edit'
+  put '/articles/:id' => 'articles#update', as: 'article_update'
   post '/articles' => 'articles#create', as: 'article_create'
 end
